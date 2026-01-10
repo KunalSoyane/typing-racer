b77e92db04d0be279a1309a39ae0ff8ba1cd63ed
Kunal vivek Soyane <kunalviveksoyane@gmail.com>
2026-01-10 21:18:21 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 import { useEffect, useState, useRef } from 'react';
 
 // CHANGE THIS TO YOUR RENDER URL IF DEPLOYING
-const socket = io.connect("http://localhost:3001"); 
+const socket = io.connect("typing-racer-git-main-kunal-soyanes-projects.vercel.app"); 
 
 function App() {
   const [room, setRoom] = useState("");
