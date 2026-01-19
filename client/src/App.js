483c6cdbe7bcf1d8a351c1a36667a3eedf53e3d9
Kunal vivek Soyane <kunalviveksoyane@gmail.com>
2026-01-19 16:48:56 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -115,7 +115,7 @@ function App() {
 
   const handleAdminLogin = () => {
     const password = prompt("Enter Admin Password:");
-    if (password === "W") {
+    if (password === "ITSA2026") {
         fetchHistory();
     } else if (password !== null) {
         alert("❌ Access Denied: Wrong Password");
